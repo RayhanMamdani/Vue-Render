bf4c2f4d0009cf864579c0465fc778108f83805f
RayhanMamdani <rmamdani@bayviewglen.ca>
2023-02-14 03:01:19 -0500

    ...

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -15,6 +15,12 @@ const router = createRouter({
       name: 'about',
       component: AboutView,
     },
+    {
+      props: true,
+      path: '/event/:id',
+      name: 'event-details',
+      component: EventViewDetails
+    }
   ],
 })
 
